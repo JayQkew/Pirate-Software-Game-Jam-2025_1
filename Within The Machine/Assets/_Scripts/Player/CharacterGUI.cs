cd6d9ee00fc3871c0c1a360b7e09c390398757e6
JayQkew <rayjebb@gmail.com>
2025-01-27 12:02:54 +0200

    tilting body

--- Within The Machine/Assets/_Scripts/Player/CharacterGUI.cs
+++ Within The Machine/Assets/_Scripts/Player/CharacterGUI.cs
@@ -1,33 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class CharacterGUI : MonoBehaviour
 {
+    [SerializeField] private MovementScript movementScript;
+    [Space(10)]
+    
     [SerializeField] private GameObject RightIKTarget;
     [SerializeField] private bool rightHolding;
     [SerializeField] private GameObject LeftIKTarget;    
     [SerializeField] private bool leftHolding;
 
     [SerializeField] private Transform holdingTarget;
-    [SerializeField] private Vector2 rightTargetPositionOffset = new Vector2(2f, 1f);
-    [SerializeField] private Vector2 leftTargetPositionOffset = new Vector2(-2f, 1f);
+    [SerializeField] private Transform rightRestingPosition;
+    [SerializeField] private Transform leftRestingPosition;
     [SerializeField] private float movementSpeed = 5f; // Speed of the smooth movement
+    
+    [Space(10)]
+    
+    [SerializeField] private GameObject bodyBone;
+    [SerializeField] private float maxTiltAngle;
+    [SerializeField] private float restAngle;
+    [SerializeField] private float tiltSpeed;
 
     void Update()
     {
         MoveIKTargets();
+        TiltGUI();
     }
 
     private void MoveIKTargets()
     {
         // Calculate target positions relative to the character
-        Vector2 rightTargetPosition = !rightHolding ? (Vector2)transform.position + rightTargetPositionOffset : holdingTarget.position;
-        Vector2 leftTargetPosition = !leftHolding ? (Vector2)transform.position + leftTargetPositionOffset : holdingTarget.position;
+        Vector2 rightTargetPosition = !rightHolding ? rightRestingPosition.position : holdingTarget.position;
+        Vector2 leftTargetPosition = !leftHolding ? leftRestingPosition.position : holdingTarget.position;
 
         // Smoothly move the IK targets toward their respective positions
         RightIKTarget.transform.position = Vector2.Lerp(RightIKTarget.transform.position, rightTargetPosition, Time.deltaTime * movementSpeed);
         LeftIKTarget.transform.position = Vector2.Lerp(LeftIKTarget.transform.position, leftTargetPosition, Time.deltaTime * movementSpeed);
+    }
+
+    private void TiltGUI()
+    {
+        // Read input
+        Vector2 inputVector = movementScript.controls.Player.Movement.ReadValue<Vector2>();
+        float horizontalInput = inputVector.x;
+
+        // Calculate target rotation angle based on horizontal input
+        float targetAngle = horizontalInput == 0 ? restAngle : horizontalInput > 0 ? -maxTiltAngle : maxTiltAngle;
+
+        // Smoothly interpolate to the target angle
+        Vector3 targetRotation = new Vector3(0f, 0f, targetAngle);
+        bodyBone.transform.eulerAngles = Vector3.Lerp(bodyBone.transform.eulerAngles, targetRotation, Time.deltaTime * tiltSpeed);
     }
 }
