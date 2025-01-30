86140152573ed35cba1e7dde8dc9331d017511d4
saifrancis <131270386+saifrancis@users.noreply.github.com>
2025-01-30 11:27:02 +0200

    hand items

--- Within The Machine/Assets/_Scripts/Player/PlayerInventory.cs
+++ Within The Machine/Assets/_Scripts/Player/PlayerInventory.cs
@@ -6,9 +6,18 @@ public class PlayerInventory : MonoBehaviour
 {
     public Inventory inventory;
 
+    public GameObject leftItem;
+    public GameObject rightItem;
+
     public bool PickupItem(ItemSlot item)
     {
-        return inventory.AddItemToInventory(item);
+        if (inventory.AddItemToInventory(item))
+        {
+            UpdateHandItems();
+            return true;
+        }
+
+        return false;
     }
 
     public void DropItem(int index)
@@ -23,6 +32,32 @@ public class PlayerInventory : MonoBehaviour
     public void PutInCraftingStation(CraftingStation craftingStation)
     {
         //
+    }
+
+    void UpdateHandItems()
+    {
+        ItemSlot item = inventory.itemsInInventory[0];
+        if (item.isEmpty())
+        {
+            leftItem.SetActive(false);
+        }
+        else
+        {
+            leftItem.SetActive(true);
+            leftItem.GetComponent<SpriteRenderer>().sprite = item.itemData.icon;
+        }
+
+        item = inventory.itemsInInventory[1];
+        if (item.isEmpty())
+        {
+            rightItem.SetActive(false);
+        }
+        else
+        {
+            rightItem.SetActive(true);
+            rightItem.GetComponent<SpriteRenderer>().sprite = item.itemData.icon;
+        }
+
     }
     
 }
